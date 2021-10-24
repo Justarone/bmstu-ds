a9232e8ecb429f9e476c806a66e06f38e1395a34
Perestoronin Pavel <p_perestoronin@mail.ru>
2021-10-24 22:27:27 +0300

    Add example script of usage

---
+++ lab_04/run.sh
@@ -0,0 +1,8 @@
+./target/release/rsa --generate_keys -o id_rsa
+./target/release/rsa -i Cargo.toml -o cipher.txt -k id_rsa.pub
+./target/release/rsa -i cipher.txt -o new.toml -k id_rsa --decode
+echo "BEFORE"
+cat Cargo.toml
+echo -e "\n\nAFTER"
+cat new.toml
+rm -f cipher.txt id_rsa id_rsa.pub new.toml
